1b4c22e1fb0e9ba801285902c319a3b292a1a31e
Mathias Kalb <mkalb@mkalb.de>
2016-04-23 21:54:15 +0200

    Add record implementations

---
+++ src/main/java/org/textfiledatatools/core/record/KeyValueRecord.java
@@ -0,0 +1,49 @@
+package org.textfiledatatools.core.record;
+
+import org.textfiledatatools.core.Field;
+
+import java.util.Objects;
+
+/**
+ * @author Mathias Kalb
+ * @since 0.1
+ */
+public class KeyValueRecord extends PairRecord implements KeyRecord, ValueRecord {
+
+    private static final long serialVersionUID = 1L;
+
+    public KeyValueRecord(String key, String value) {
+        super(key, value);
+        Objects.requireNonNull(key);
+    }
+
+    public KeyValueRecord(String category, Long recordId, String key, String value) {
+        super(category, recordId, key, value);
+        Objects.requireNonNull(key);
+    }
+
+    @Override
+    public KeyValueRecord newValueRecord(String value) {
+        return new KeyValueRecord(getCategory(), getRecordId(), getValueOfKeyField(), value);
+    }
+
+    @Override
+    public final Field getKeyField() {
+        return getFirstField();
+    }
+
+    @Override
+    public final Field getValueField() {
+        return getSecondField();
+    }
+
+    @Override
+    public String toString() {
+        return "KeyValueRecord{" +
+                "category=" + getCategory() +
+                ", recordId=" + getRecordId() +
+                ", key=" + getFirstField().getValue() +
+                ", value=" + getSecondField().getValue() +
+                '}';
+    }
+}
---
+++ src/main/java/org/textfiledatatools/core/record/StandardRecord.java
@@ -0,0 +1,122 @@
+package org.textfiledatatools.core.record;
+
+import org.textfiledatatools.core.Field;
+import org.textfiledatatools.core.Record;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author Mathias Kalb
+ * @since 0.1
+ */
+public class StandardRecord implements Record {
+
+    private static final long serialVersionUID = 1L;
+
+    private final String category;
+    private final Long recordId;
+    private final Field[] fields;
+
+    public StandardRecord() {
+        this(null, null, 0);
+    }
+
+    public StandardRecord(Collection<String> values) {
+        this(null, null, values);
+    }
+
+    public StandardRecord(String category, Long recordId, Collection<String> values) {
+        this(category, recordId, values.size());
+        int index = 0;
+        for (String value : values) {
+            fields[index] = new Field(index, index + 1 == values.size(), value);
+            index++;
+        }
+    }
+
+    public StandardRecord(String... values) {
+        this(null, null, values);
+    }
+
+    public StandardRecord(String category, Long recordId, String... values) {
+        this(category, recordId, values.length);
+        for (int index = 0; index < values.length; index++) {
+            fields[index] = new Field(index, index + 1 == values.length, values[index]);
+        }
+    }
+
+    private StandardRecord(String category, Long recordId, int size) {
+        this.category = category;
+        this.recordId = recordId;
+        fields = new Field[size];
+    }
+
+    @Override
+    public final Field[] toNewArray() {
+        synchronized (fields) {
+            return Arrays.copyOf(fields, fields.length);
+        }
+    }
+
+    @Override
+    public final List<Field> toNewList() {
+        return Arrays.asList(toNewArray());
+    }
+
+    @Override
+    public final Stream<Field> toNewStream() {
+        return Arrays.stream(toNewArray());
+    }
+
+    @Override
+    public final String getCategory() {
+        return category;
+    }
+
+    @Override
+    public final Long getRecordId() {
+        return recordId;
+    }
+
+    @Override
+    public final int size() {
+        return fields.length;
+    }
+
+    @Override
+    public final Field getFieldAt(int index) {
+        return ((index >= 0) && (index < fields.length)) ? fields[index] : null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StandardRecord that = (StandardRecord) o;
+        return Objects.equals(category, that.category) &&
+                Arrays.equals(fields, that.fields);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(category, fields);
+    }
+
+    protected final String getConcatenatedValues() {
+        return Arrays.stream(fields).map(Field::getValue).collect(Collectors.joining(", "));
+    }
+
+    @Override
+    public String toString() {
+        return "StandardRecord{" +
+                "category=" + category +
+                ", recordId=" + recordId +
+                ", values=[" + getConcatenatedValues() +
+                "]}";
+    }
+}
