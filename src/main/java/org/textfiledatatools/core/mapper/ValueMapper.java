80c041499178143ef08660e2a30dd896123c73f9
Mathias Kalb <mkalb@mkalb.de>
2016-05-05 22:42:01 +0200

    Add more RecordMappers

---
+++ src/main/java/org/textfiledatatools/core/mapper/ValueMapper.java
@@ -0,0 +1,31 @@
+package org.textfiledatatools.core.mapper;
+
+import org.textfiledatatools.core.mapper.fieldvalue.ConstantFieldValueMapper;
+import org.textfiledatatools.core.mapper.fieldvalue.FieldValueMapper;
+import org.textfiledatatools.core.record.ValueRecord;
+
+import java.util.Objects;
+
+/**
+ * @author Mathias Kalb
+ * @since 0.1
+ */
+public class ValueMapper implements UnaryRecordMapper<ValueRecord> {
+
+    private final FieldValueMapper fieldValueMapper;
+
+    public ValueMapper(String constantValue) {
+        this(new ConstantFieldValueMapper(constantValue));
+    }
+
+    public ValueMapper(FieldValueMapper fieldValueMapper) {
+        Objects.requireNonNull(fieldValueMapper);
+        this.fieldValueMapper = fieldValueMapper;
+    }
+
+    @Override
+    public ValueRecord map(ValueRecord record) {
+        return record.newValueRecord(fieldValueMapper.mapToValue(record.getValueField()));
+    }
+
+}
---
+++ src/main/java/org/textfiledatatools/core/mapper/ValuesMapper.java
@@ -0,0 +1,32 @@
+package org.textfiledatatools.core.mapper;
+
+import org.textfiledatatools.core.Fields;
+import org.textfiledatatools.core.Record;
+import org.textfiledatatools.core.mapper.fieldvalue.FieldValueMapper;
+import org.textfiledatatools.core.record.StandardRecord;
+
+import java.util.Objects;
+
+/**
+ * @author Mathias Kalb
+ * @since 0.1
+ */
+public class ValuesMapper implements RecordMapper<Record, Record> {
+
+    private final FieldValueMapper fieldValueMapper;
+
+    public ValuesMapper(FieldValueMapper fieldValueMapper) {
+        Objects.requireNonNull(fieldValueMapper);
+        this.fieldValueMapper = fieldValueMapper;
+    }
+
+    @Override
+    public Record map(Record record) {
+        if (record.isEmpty()) {
+            return record;
+        }
+        return new StandardRecord(record.getCategory(), record.getRecordId(),
+                Fields.collectValues(record, fieldValueMapper));
+    }
+
+}
