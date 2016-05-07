4408791907cd3c6d7b9322c3e48530738cb20327
Mathias Kalb <mkalb@mkalb.de>
2016-05-07 14:40:33 +0200

    Add a FieldValueMapper

---
+++ src/main/java/org/textfiledatatools/core/mapper/fieldvalue/StringOperationFieldValueMapper.java
@@ -0,0 +1,33 @@
+package org.textfiledatatools.core.mapper.fieldvalue;
+
+import org.textfiledatatools.core.Field;
+import org.textfiledatatools.util.StringOperation;
+
+import java.util.Locale;
+import java.util.Objects;
+
+/**
+ * @author Mathias Kalb
+ * @since 0.1
+ */
+public class StringOperationFieldValueMapper implements FieldValueMapper {
+
+    protected final StringOperation stringOperation;
+    protected final Locale locale;
+
+    public StringOperationFieldValueMapper(StringOperation stringOperation) {
+        this(stringOperation, null);
+    }
+
+    public StringOperationFieldValueMapper(StringOperation stringOperation, Locale locale) {
+        Objects.requireNonNull(stringOperation);
+        this.stringOperation = stringOperation;
+        this.locale = locale;
+    }
+
+    @Override
+    public String mapToValue(Field field) {
+        return StringOperation.operate(field.getValue(), stringOperation, locale);
+    }
+
+}
