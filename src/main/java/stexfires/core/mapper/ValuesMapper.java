2e4d7177d2cd6e77804c33f4c671026a9b310ef3
Mathias Kalb <mkalb@mkalb.de>
2016-07-03 21:05:57 +0200

    Enhance ValuesMapper

--- src/main/java/stexfires/core/mapper/ValuesMapper.java
+++ src/main/java/stexfires/core/mapper/ValuesMapper.java
@@ -1,11 +1,15 @@
 package stexfires.core.mapper;
 
+import stexfires.core.Field;
 import stexfires.core.Fields;
 import stexfires.core.Record;
 import stexfires.core.mapper.fieldvalue.FieldValueMapper;
 import stexfires.core.record.StandardRecord;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author Mathias Kalb
@@ -13,20 +17,31 @@ import java.util.Objects;
  */
 public class ValuesMapper implements UnaryRecordMapper<Record> {
 
-    protected final FieldValueMapper fieldValueMapper;
+    protected final Function<Record, List<String>> valuesFunction;
 
-    public ValuesMapper(FieldValueMapper fieldValueMapper) {
+    public ValuesMapper(Function<Record, List<String>> valuesFunction) {
+        Objects.requireNonNull(valuesFunction);
+        this.valuesFunction = valuesFunction;
+    }
+
+    public static ValuesMapper getInstance(List<Function<Record, String>> functions) {
+        Objects.requireNonNull(functions);
+        return new ValuesMapper(r -> functions.stream().map(f -> f.apply(r)).collect(Collectors.toList()));
+    }
+
+    public static ValuesMapper getInstance(Function<List<Field>, List<String>> function) {
+        Objects.requireNonNull(function);
+        return new ValuesMapper(r -> function.apply(r.listOfFields()));
+    }
+
+    public static ValuesMapper getInstance(FieldValueMapper fieldValueMapper) {
         Objects.requireNonNull(fieldValueMapper);
-        this.fieldValueMapper = fieldValueMapper;
+        return new ValuesMapper(r -> Fields.collectValues(r, fieldValueMapper));
     }
 
     @Override
     public Record map(Record record) {
-        if (record.isEmpty()) {
-            return record;
-        }
-        return new StandardRecord(record.getCategory(), record.getRecordId(),
-                Fields.collectValues(record, fieldValueMapper));
+        return new StandardRecord(record.getCategory(), record.getRecordId(), valuesFunction.apply(record));
     }
 
 }
