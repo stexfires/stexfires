bf1df97909cf08e1971cda6abdabe20e443cb971
Mathias Kalb <mkalb@mkalb.de>
2016-07-07 22:36:19 +0200

    Add more static methods

--- src/main/java/stexfires/core/modifier/GroupModifier.java
+++ src/main/java/stexfires/core/modifier/GroupModifier.java
@@ -3,15 +3,21 @@ package stexfires.core.modifier;
 import stexfires.core.Record;
 import stexfires.core.message.RecordMessage;
 import stexfires.core.record.KeyRecord;
+import stexfires.core.record.SingleRecord;
+import stexfires.core.record.StandardRecord;
+import stexfires.core.record.ValueRecord;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
+import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
+ * Aggregator / Group By.
+ *
  * @author Mathias Kalb
  * @since 0.1
  */
@@ -49,8 +55,35 @@ public class GroupModifier<T extends Record, R extends Record> implements Record
         return KeyRecord::getValueOfKeyField;
     }
 
-    public static <T extends Record> Predicate<List<? super T>> sizeGreaterThan(int size) {
+    public static <T extends ValueRecord> Function<? super T, String> groupByValue() {
+        return ValueRecord::getValueOfValueField;
+    }
+
+    public static <T extends Record> Function<? super T, String> groupByValue(int index) {
+        return r -> r.getValueAt(index);
+    }
+
+    public static <T extends Record> Predicate<List<? super T>> havingSize(IntPredicate sizePredicate) {
+        Objects.requireNonNull(sizePredicate);
+        return list -> sizePredicate.test(list.size());
+    }
+
+    public static <T extends Record> Predicate<List<? super T>> havingSizeGreaterThan(int size) {
         return list -> list.size() > size;
+    }
+
+    public static <T extends Record> Function<List<T>, ValueRecord> aggregateToValue(Function<List<T>, String> categoryFunction,
+                                                                                     Function<List<T>, String> valueFunction) {
+        Objects.requireNonNull(categoryFunction);
+        Objects.requireNonNull(valueFunction);
+        return list -> new SingleRecord(categoryFunction.apply(list), null, valueFunction.apply(list));
+    }
+
+    public static <T extends Record> Function<List<T>, Record> aggregateToValues(Function<List<T>, String> categoryFunction,
+                                                                                 Function<List<T>, List<String>> valuesFunction) {
+        Objects.requireNonNull(categoryFunction);
+        Objects.requireNonNull(valuesFunction);
+        return list -> new StandardRecord(categoryFunction.apply(list), null, valuesFunction.apply(list));
     }
 
     @Override
