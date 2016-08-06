bae49e494e51af00b20b6480db8032b3f3f5cb4a
Mathias Kalb <mkalb@mkalb.de>
2016-08-06 23:09:03 +0200

    Add pivot methods

--- src/main/java/stexfires/core/modifier/GroupModifier.java
+++ src/main/java/stexfires/core/modifier/GroupModifier.java
@@ -1,5 +1,6 @@
 package stexfires.core.modifier;
 
+import stexfires.core.Field;
 import stexfires.core.Record;
 import stexfires.core.message.RecordMessage;
 import stexfires.core.record.KeyRecord;
@@ -7,8 +8,7 @@ import stexfires.core.record.SingleRecord;
 import stexfires.core.record.StandardRecord;
 import stexfires.core.record.ValueRecord;
 
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.IntPredicate;
 import java.util.function.Predicate;
@@ -79,11 +79,138 @@ public class GroupModifier<T extends Record, R extends Record> implements Record
         return list -> new SingleRecord(categoryFunction.apply(list), null, valueFunction.apply(list));
     }
 
+    public static <T extends Record> Function<List<T>, ValueRecord> aggregateToValue(Function<List<T>, String> valueFunction) {
+        Objects.requireNonNull(valueFunction);
+        return list -> new SingleRecord(valueFunction.apply(list));
+    }
+
     public static <T extends Record> Function<List<T>, Record> aggregateToValues(Function<List<T>, String> categoryFunction,
                                                                                  Function<List<T>, List<String>> valuesFunction) {
         Objects.requireNonNull(categoryFunction);
         Objects.requireNonNull(valuesFunction);
         return list -> new StandardRecord(categoryFunction.apply(list), null, valuesFunction.apply(list));
+    }
+
+    public static <T extends Record> Function<List<T>, Record> aggregateToValues(Function<List<T>, List<String>> valuesFunction) {
+        Objects.requireNonNull(valuesFunction);
+        return list -> new StandardRecord(valuesFunction.apply(list));
+    }
+
+    public static <T extends Record> Function<List<T>, List<String>> maxValuesFunction(String nullValue) {
+        return list -> list.stream()
+                .map(Record::streamOfFields)
+                .flatMap(Function.identity())
+                .collect(Collectors.collectingAndThen(
+                        Collectors.groupingBy(Field::getIndex, TreeMap::new,
+                                Collectors.mapping(Field::getValue,
+                                        Collectors.maxBy(Comparator.nullsFirst(Comparator.naturalOrder())))),
+                        map -> map.values().stream().map(o -> o.orElse(nullValue)).collect(Collectors.toList())));
+    }
+
+    public static <T extends Record> GroupModifier<T, Record> pivotModifier(int keyIndex,
+                                                                            int maxRecordsPerGroup,
+                                                                            String nullValue,
+                                                                            Integer... valueIndexes) {
+        return pivotModifier(keyIndex, maxRecordsPerGroup, nullValue, Arrays.asList(valueIndexes));
+    }
+
+    public static <T extends Record> GroupModifier<T, Record> pivotModifier(int keyIndex,
+                                                                            int maxRecordsPerGroup,
+                                                                            String nullValue,
+                                                                            List<Integer> valueIndexes) {
+        if (maxRecordsPerGroup < 0) {
+            throw new IllegalArgumentException("maxRecordsPerGroup=" + maxRecordsPerGroup);
+        }
+        Objects.requireNonNull(valueIndexes);
+        int newRecordSize = 1 + maxRecordsPerGroup * valueIndexes.size();
+        return new GroupModifier<>(r -> r.getValueAt(keyIndex), GroupModifier.aggregateToValues(
+                GroupModifier.pivotValuesFunction(
+                        r -> Stream.of(r.getValueAt(keyIndex)),
+                        newRecordSize,
+                        nullValue,
+                        valueIndexes)));
+    }
+
+    public static <T extends Record> Function<List<T>, List<String>> pivotValuesFunction(
+            Function<T, Stream<String>> newValuesOfFirstRecord,
+            int newRecordSize,
+            String nullValue,
+            List<Integer> valueIndexes) {
+        Objects.requireNonNull(newValuesOfFirstRecord);
+        if (newRecordSize < 0) {
+            throw new IllegalArgumentException("newRecordSize=" + newRecordSize);
+        }
+        Objects.requireNonNull(valueIndexes);
+        return list -> Stream.concat(Stream.concat(
+                newValuesOfFirstRecord.apply(list.get(0)),
+                list.stream()
+                        .map(r -> valueIndexes.stream().map(i -> r.getValueAtOrElse(i, nullValue)))
+                        .flatMap(Function.identity())),
+                Stream.generate(() -> nullValue))
+                .limit(newRecordSize)
+                .collect(Collectors.toList());
+    }
+
+    public static <T extends Record> GroupModifier<T, Record> pivotModifier(int keyIndex,
+                                                                            int valueIndex,
+                                                                            String nullValue,
+                                                                            int valueClassIndex,
+                                                                            String... valueClasses) {
+        return pivotModifier(keyIndex, valueIndex, nullValue, valueClassIndex, Arrays.asList(valueClasses));
+    }
+
+    public static <T extends Record> GroupModifier<T, Record> pivotModifier(int keyIndex,
+                                                                            int valueIndex,
+                                                                            String nullValue,
+                                                                            int valueClassIndex,
+                                                                            List<String> valueClasses) {
+        Objects.requireNonNull(valueClasses);
+        return pivotModifier(
+                r -> r.getValueAt(keyIndex),
+                r -> r.getValueAt(valueIndex),
+                nullValue,
+                r -> r.getValueAt(valueClassIndex),
+                valueClasses);
+    }
+
+    public static <T extends Record> GroupModifier<T, Record> pivotModifier(Function<T, String> newKey,
+                                                                            Function<T, String> newValue,
+                                                                            String nullValue,
+                                                                            Function<T, String> valueClass,
+                                                                            List<String> valueClasses) {
+        Objects.requireNonNull(newKey);
+        Objects.requireNonNull(newValue);
+        Objects.requireNonNull(valueClass);
+        Objects.requireNonNull(valueClasses);
+        return new GroupModifier<>(newKey,
+                GroupModifier.aggregateToValues(GroupModifier.pivotValuesFunction(
+                        r -> Stream.of(newKey.apply(r)),
+                        newValue,
+                        nullValue,
+                        valueClass,
+                        valueClasses
+                )));
+    }
+
+    public static <T extends Record> Function<List<T>, List<String>> pivotValuesFunction(
+            Function<T, Stream<String>> newValuesOfFirstRecord,
+            Function<T, String> newValueOfEveryRecord,
+            String nullValue,
+            Function<T, String> valueClass,
+            List<String> valueClasses) {
+        Objects.requireNonNull(newValuesOfFirstRecord);
+        Objects.requireNonNull(newValueOfEveryRecord);
+        Objects.requireNonNull(valueClass);
+        Objects.requireNonNull(valueClasses);
+        return list -> Stream.concat(
+                newValuesOfFirstRecord.apply(list.get(0)),
+                valueClasses.stream().map(vc -> list.stream()
+                        .filter(r -> vc.equals(valueClass.apply(r)))
+                        .map(newValueOfEveryRecord)
+                        .map(v -> v == null ? nullValue : v)
+                        .findFirst()
+                        .orElse(nullValue)))
+                .collect(Collectors.toList());
     }
 
     @Override
