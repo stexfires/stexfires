257b4b8a115677fe7b2a53856d2b8d390e8ad128
Mathias Kalb <mkalb@mkalb.de>
2016-10-09 14:30:20 +0200

    Add CollectionConsumer and MapConsumer

---
+++ src/main/java/stexfires/core/consumer/CollectionConsumer.java
@@ -0,0 +1,34 @@
+package stexfires.core.consumer;
+
+import stexfires.core.record.ValueRecord;
+
+import java.util.Collection;
+import java.util.Objects;
+
+/**
+ * @author Mathias Kalb
+ * @since 0.1
+ */
+public class CollectionConsumer<T extends Collection<String>> implements RecordConsumer<ValueRecord> {
+
+    protected final Object lock = new Object();
+
+    protected final T collection;
+
+    public CollectionConsumer(T collection) {
+        Objects.requireNonNull(collection);
+        this.collection = collection;
+    }
+
+    @Override
+    public void consume(ValueRecord record) throws UncheckedConsumerException {
+        synchronized (lock) {
+            collection.add(record.getValueOfValueField());
+        }
+    }
+
+    public T getCollection() {
+        return collection;
+    }
+
+}
---
+++ src/main/java/stexfires/core/consumer/MapConsumer.java
@@ -0,0 +1,34 @@
+package stexfires.core.consumer;
+
+import stexfires.core.record.KeyValueRecord;
+
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @author Mathias Kalb
+ * @since 0.1
+ */
+public class MapConsumer<T extends Map<String, String>> implements RecordConsumer<KeyValueRecord> {
+
+    protected final Object lock = new Object();
+
+    protected final T map;
+
+    public MapConsumer(T map) {
+        Objects.requireNonNull(map);
+        this.map = map;
+    }
+
+    @Override
+    public void consume(KeyValueRecord record) throws UncheckedConsumerException {
+        synchronized (lock) {
+            map.put(record.getValueOfKeyField(), record.getValueOfValueField());
+        }
+    }
+
+    public T getMap() {
+        return map;
+    }
+
+}
