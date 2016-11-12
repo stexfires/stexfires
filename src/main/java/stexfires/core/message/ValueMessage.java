1d6e5c252aa34d4571063fe769e22dd9c7f070d6
Mathias Kalb <mkalb@mkalb.de>
2016-11-12 15:07:08 +0100

    Refactor ValueMessage

--- src/main/java/stexfires/core/message/ValueMessage.java
+++ src/main/java/stexfires/core/message/ValueMessage.java
@@ -1,16 +1,49 @@
 package stexfires.core.message;
 
-import stexfires.core.record.ValueRecord;
+import stexfires.core.Field;
+import stexfires.core.Record;
+
+import java.util.Objects;
+import java.util.function.Function;
 
 /**
  * @author Mathias Kalb
  * @since 0.1
  */
-public class ValueMessage<T extends ValueRecord> implements RecordMessage<T> {
+public class ValueMessage<T extends Record> implements RecordMessage<T> {
+
+    public static final String DEFAULT_NULL_FIELD_VALUE = null;
+
+    protected final Function<? super T, Field> fieldFunction;
+    protected final String nullFieldValue;
+
+    public ValueMessage(int index) {
+        this(record -> record.getFieldAt(index), DEFAULT_NULL_FIELD_VALUE);
+    }
+
+    public ValueMessage(int index,
+                        String nullFieldValue) {
+        this(record -> record.getFieldAt(index), nullFieldValue);
+    }
+
+    public ValueMessage(Function<? super T, Field> fieldFunction) {
+        this(fieldFunction, null);
+    }
+
+    public ValueMessage(Function<? super T, Field> fieldFunction,
+                        String nullFieldValue) {
+        Objects.requireNonNull(fieldFunction);
+        this.fieldFunction = fieldFunction;
+        this.nullFieldValue = nullFieldValue;
+    }
 
     @Override
     public String createMessage(T record) {
-        return record.getValueOfValueField();
+        Field field = fieldFunction.apply(record);
+        if (field == null) {
+            return nullFieldValue;
+        }
+        return field.getValue();
     }
 
 }
