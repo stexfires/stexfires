097f2e26ae027d4f44b691d0d2311a0ade47553f
Mathias Kalb <mkalb@mkalb.de>
2016-11-12 15:44:12 +0100

    Add ConditionalMessage

---
+++ src/main/java/stexfires/core/message/ConditionalMessage.java
@@ -0,0 +1,34 @@
+package stexfires.core.message;
+
+import stexfires.core.Record;
+import stexfires.core.filter.RecordFilter;
+
+import java.util.Objects;
+
+/**
+ * @author Mathias Kalb
+ * @since 0.1
+ */
+public class ConditionalMessage<T extends Record> implements RecordMessage<T> {
+
+    protected final RecordFilter<? super T> condition;
+    protected final RecordMessage<? super Record> trueMessage;
+    protected final RecordMessage<? super Record> falseMessage;
+
+    public ConditionalMessage(RecordFilter<? super T> condition,
+                              RecordMessage<? super Record> trueMessage,
+                              RecordMessage<? super Record> falseMessage) {
+        Objects.requireNonNull(condition);
+        Objects.requireNonNull(trueMessage);
+        Objects.requireNonNull(falseMessage);
+        this.condition = condition;
+        this.trueMessage = trueMessage;
+        this.falseMessage = falseMessage;
+    }
+
+    @Override
+    public String createMessage(T record) {
+        return condition.isValid(record) ? trueMessage.createMessage(record) : falseMessage.createMessage(record);
+    }
+
+}
