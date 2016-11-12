b132558bfd4dad1b2f399d608ab64a250e0aacac
Mathias Kalb <mkalb@mkalb.de>
2016-11-12 14:47:43 +0100

    Add optional hashCode

--- src/main/java/stexfires/core/message/ClassNameMessage.java
+++ src/main/java/stexfires/core/message/ClassNameMessage.java
@@ -8,9 +8,23 @@ import stexfires.core.Record;
  */
 public class ClassNameMessage<T extends Record> implements RecordMessage<T> {
 
+    public static final boolean DEFAULT_WITH_HASH_CODE = false;
+
+    protected final boolean withHashCode;
+
+    public ClassNameMessage() {
+        this(DEFAULT_WITH_HASH_CODE);
+    }
+
+    public ClassNameMessage(boolean withHashCode) {
+        this.withHashCode = withHashCode;
+    }
+
     @Override
     public String createMessage(T record) {
-        return record.getClass().getName();
+        return withHashCode
+                ? record.getClass().getName() + "@" + Integer.toHexString(record.hashCode())
+                : record.getClass().getName();
     }
 
 }
