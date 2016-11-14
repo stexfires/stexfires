1d4e734e45b3151a1245e965010a3b40f908fe6a
Mathias Kalb <mkalb@mkalb.de>
2016-11-14 21:29:37 +0100

    Rename ValuesMessage

--- src/main/java/stexfires/core/message/JoinedValuesMessage.java
+++ src/main/java/stexfires/core/message/JoinedValuesMessage.java
@@ -9,15 +9,15 @@ import java.util.Objects;
  * @author Mathias Kalb
  * @since 0.1
  */
-public class ValuesMessage<T extends Record> implements RecordMessage<T> {
+public class JoinedValuesMessage<T extends Record> implements RecordMessage<T> {
 
     protected final CharSequence delimiter;
 
-    public ValuesMessage() {
+    public JoinedValuesMessage() {
         this(Fields.DEFAULT_FIELD_VALUE_DELIMITER);
     }
 
-    public ValuesMessage(CharSequence delimiter) {
+    public JoinedValuesMessage(CharSequence delimiter) {
         Objects.requireNonNull(delimiter);
         this.delimiter = delimiter;
     }
