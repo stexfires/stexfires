b334380e24a581fae3fe3100726fc515f2c8cf48
Mathias Kalb <mkalb@mkalb.de>
2016-12-10 15:16:41 +0100

    Refactor PropertiesFileSpec

--- src/main/java/stexfires/io/properties/PropertiesFileSpec.java
+++ src/main/java/stexfires/io/properties/PropertiesFileSpec.java
@@ -4,6 +4,7 @@ import stexfires.util.LineSeparator;
 
 import java.nio.charset.Charset;
 import java.nio.charset.CodingErrorAction;
+import java.nio.file.Path;
 import java.util.Objects;
 
 /**
@@ -16,31 +17,55 @@ public final class PropertiesFileSpec {
     public static final String COMMENT_PREFIX = "#";
     public static final String NULL_VALUE = "";
 
-    private final Charset charset;
+    public static final CodingErrorAction DEFAULT_CODING_ERROR_ACTION = CodingErrorAction.REPORT;
+    public static final boolean DEFAULT_DATE_COMMENT = false;
 
-    private final LineSeparator lineSeparator;
-    private final boolean escapeUnicode;
-    private final boolean dateComment;
+    private final Charset charset;
     private final CodingErrorAction codingErrorAction;
 
-    public PropertiesFileSpec(Charset charset, LineSeparator lineSeparator, boolean escapeUnicode, boolean dateComment,
-                              CodingErrorAction codingErrorAction) {
+    private final boolean escapeUnicode;
+    private final boolean dateComment;
+    private final LineSeparator lineSeparator;
+
+    public PropertiesFileSpec(Charset charset, CodingErrorAction codingErrorAction,
+                              boolean escapeUnicode, boolean dateComment,
+                              LineSeparator lineSeparator) {
         Objects.requireNonNull(charset);
-        Objects.requireNonNull(lineSeparator);
         Objects.requireNonNull(codingErrorAction);
+        Objects.requireNonNull(lineSeparator);
         this.charset = charset;
-        this.lineSeparator = lineSeparator;
+        this.codingErrorAction = codingErrorAction;
         this.escapeUnicode = escapeUnicode;
         this.dateComment = dateComment;
-        this.codingErrorAction = codingErrorAction;
+        this.lineSeparator = lineSeparator;
+    }
+
+    public static PropertiesFileSpec write(Charset charset,
+                                           boolean escapeUnicode,
+                                           LineSeparator lineSeparator) {
+        return new PropertiesFileSpec(charset, DEFAULT_CODING_ERROR_ACTION,
+                escapeUnicode, DEFAULT_DATE_COMMENT,
+                lineSeparator);
+    }
+
+    public static PropertiesFileSpec write(Charset charset, CodingErrorAction codingErrorAction,
+                                           boolean escapeUnicode, boolean dateComment,
+                                           LineSeparator lineSeparator) {
+        return new PropertiesFileSpec(charset, codingErrorAction,
+                escapeUnicode, dateComment,
+                lineSeparator);
+    }
+
+    public PropertiesFile file(Path path) {
+        return new PropertiesFile(path, this);
     }
 
     public Charset getCharset() {
         return charset;
     }
 
-    public LineSeparator getLineSeparator() {
-        return lineSeparator;
+    public CodingErrorAction getCodingErrorAction() {
+        return codingErrorAction;
     }
 
     public boolean isEscapeUnicode() {
@@ -51,8 +76,8 @@ public final class PropertiesFileSpec {
         return dateComment;
     }
 
-    public CodingErrorAction getCodingErrorAction() {
-        return codingErrorAction;
+    public LineSeparator getLineSeparator() {
+        return lineSeparator;
     }
 
 }
