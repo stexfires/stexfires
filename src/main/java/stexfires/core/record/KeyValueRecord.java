629abe1d14a4296f49c8abd1ffc15c6b0ec3c4e6
Mathias Kalb <mkalb@mkalb.de>
2020-05-30 18:05:37 +0200

    Fix wrong 'final' at KeyValueRecord

--- src/main/java/stexfires/core/record/KeyValueRecord.java
+++ src/main/java/stexfires/core/record/KeyValueRecord.java
@@ -26,7 +26,7 @@ public class KeyValueRecord extends PairRecord implements KeyRecord, ValueRecord
     }
 
     @Override
-    public final KeyValueRecord newValueRecord(@Nullable String value) {
+    public KeyValueRecord newValueRecord(@Nullable String value) {
         return new KeyValueRecord(getCategory(), getRecordId(), getValueOfKeyField(), value);
     }
 
