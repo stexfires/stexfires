d4ba88e2884f49783fb1fef98192451d9405d2ea
Mathias Kalb <mkalb@mkalb.de>
2022-08-28 15:29:09 +0200

    Add method 'valueOfValueFieldAsOptional'

--- src/main/java/stexfires/record/ValueRecord.java
+++ src/main/java/stexfires/record/ValueRecord.java
@@ -3,6 +3,8 @@ package stexfires.record;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Optional;
+
 /**
  * @author Mathias Kalb
  * @since 0.1
@@ -15,6 +17,10 @@ public interface ValueRecord extends TextRecord {
 
     default @Nullable String valueOfValueField() {
         return valueField().value();
+    }
+
+    default Optional<String> valueOfValueFieldAsOptional() {
+        return Optional.ofNullable(valueField().value());
     }
 
 }
