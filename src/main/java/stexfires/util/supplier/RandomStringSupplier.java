5295f41978f67f6f3e1de9eb7a3b1438f77175ce
Mathias Kalb <mkalb@mkalb.de>
2022-09-12 21:48:18 +0200

    Enhance RandomStringSupplier #28

--- src/main/java/stexfires/util/supplier/RandomStringSupplier.java
+++ src/main/java/stexfires/util/supplier/RandomStringSupplier.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 import java.util.function.IntPredicate;
-import java.util.function.LongSupplier;
+import java.util.function.IntSupplier;
 import java.util.function.Supplier;
 import java.util.random.RandomGenerator;
 
@@ -26,6 +26,7 @@ public final class RandomStringSupplier implements Supplier<String> {
 
     public static RandomStringSupplier stringSelection(RandomGenerator random,
                                                        List<String> sourceStrings) {
+        Objects.requireNonNull(random);
         Objects.requireNonNull(sourceStrings);
         if (sourceStrings.isEmpty()) {
             throw new IllegalArgumentException("At least one string must be passed.");
@@ -44,36 +45,88 @@ public final class RandomStringSupplier implements Supplier<String> {
         return new RandomStringSupplier(() -> sourceStrings[random.nextInt(0, sourceStrings.length)]);
     }
 
-    public static RandomStringSupplier characterConcatenation(RandomGenerator random,
+    public static RandomStringSupplier characterConcatenation(RandomGenerator random, IntSupplier length,
                                                               int lowestCharacterBoundary, int highestCharacterBoundary,
-                                                              IntPredicate characterFilter, LongSupplier lengthSupplier) {
+                                                              IntPredicate characterFilter) {
         Objects.requireNonNull(random);
+        Objects.requireNonNull(length);
         Objects.requireNonNull(characterFilter);
-        Objects.requireNonNull(lengthSupplier);
         if (lowestCharacterBoundary < Character.MIN_VALUE || lowestCharacterBoundary > highestCharacterBoundary || highestCharacterBoundary > Character.MAX_VALUE) {
             throw new IllegalArgumentException("Illegal character boundaries. lowestCharacterBoundary=" + lowestCharacterBoundary + ", highestCharacterBoundary=" + highestCharacterBoundary);
         }
         return new RandomStringSupplier(() -> random.ints(lowestCharacterBoundary, highestCharacterBoundary + 1)
                                                     .filter(characterFilter)
-                                                    .limit(lengthSupplier.getAsLong())
+                                                    .limit(length.getAsInt())
                                                     .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                                                     .toString());
     }
 
-    public static RandomStringSupplier characterConcatenation(RandomGenerator random,
-                                                              String sourceCharacters,
-                                                              LongSupplier lengthSupplier) {
+    public static RandomStringSupplier characterConcatenation(RandomGenerator random, IntSupplier length,
+                                                              String sourceCharacters) {
         Objects.requireNonNull(random);
+        Objects.requireNonNull(length);
         Objects.requireNonNull(sourceCharacters);
-        Objects.requireNonNull(lengthSupplier);
         if (sourceCharacters.isEmpty()) {
             throw new IllegalArgumentException("At least one character must be passed.");
         }
         return new RandomStringSupplier(() -> random.ints(0, sourceCharacters.length())
                                                     .map(sourceCharacters::charAt)
-                                                    .limit(lengthSupplier.getAsLong())
+                                                    .limit(length.getAsInt())
                                                     .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                                                     .toString());
+    }
+
+    public static RandomStringSupplier characterConcatenation(RandomGenerator random, IntSupplier length,
+                                                              List<Character> sourceCharacters) {
+        Objects.requireNonNull(random);
+        Objects.requireNonNull(length);
+        Objects.requireNonNull(sourceCharacters);
+        if (sourceCharacters.isEmpty()) {
+            throw new IllegalArgumentException("At least one character must be passed.");
+        }
+
+        return new RandomStringSupplier(() -> random.ints(0, sourceCharacters.size())
+                                                    .map(i -> (int) sourceCharacters.get(i))
+                                                    .limit(length.getAsInt())
+                                                    .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                                                    .toString());
+    }
+
+    @SuppressWarnings("OverloadedVarargsMethod")
+    public static RandomStringSupplier characterConcatenation(RandomGenerator random, IntSupplier length,
+                                                              Character... sourceCharacters) {
+        Objects.requireNonNull(random);
+        Objects.requireNonNull(length);
+        Objects.requireNonNull(sourceCharacters);
+        if (sourceCharacters.length == 0) {
+            throw new IllegalArgumentException("At least one character must be passed.");
+        }
+        return new RandomStringSupplier(() -> random.ints(0, sourceCharacters.length)
+                                                    .map(i -> (int) sourceCharacters[i])
+                                                    .limit(length.getAsInt())
+                                                    .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                                                    .toString());
+    }
+
+    public static RandomStringSupplier stringCutting(IntSupplier beginIndex, IntSupplier length,
+                                                     String sourceString) {
+        Objects.requireNonNull(beginIndex);
+        Objects.requireNonNull(length);
+        Objects.requireNonNull(sourceString);
+        if (sourceString.isEmpty()) {
+            throw new IllegalArgumentException("At least one character must be passed.");
+        }
+        return new RandomStringSupplier(() -> {
+            int begin = beginIndex.getAsInt();
+            if (begin > sourceString.length()) {
+                begin = sourceString.length();
+            }
+            int end = begin + length.getAsInt();
+            if (end > sourceString.length()) {
+                end = sourceString.length();
+            }
+            return sourceString.substring(begin, end);
+        });
     }
 
     @Override
