1cd97c3e261cfe4b2f459405b2717de20766ec2a
mkalb <mkalb@mkalb.de>
2022-10-16 14:17:37 +0200

    Replaced 'mapAndFilter' method with a new class 'MappingFilter'

---
+++ src/main/java/stexfires/record/filter/MappingFilter.java
@@ -0,0 +1,29 @@
+package stexfires.record.filter;
+
+import stexfires.record.TextRecord;
+import stexfires.record.mapper.RecordMapper;
+
+import java.util.Objects;
+
+/**
+ * @author Mathias Kalb
+ * @since 0.1
+ */
+public class MappingFilter<T extends TextRecord, V extends TextRecord> implements RecordFilter<T> {
+
+    private final RecordMapper<? super T, ? extends V> recordMapper;
+    private final RecordFilter<? super V> recordFilter;
+
+    public MappingFilter(RecordMapper<? super T, ? extends V> recordMapper, RecordFilter<? super V> recordFilter) {
+        Objects.requireNonNull(recordMapper);
+        Objects.requireNonNull(recordFilter);
+        this.recordMapper = recordMapper;
+        this.recordFilter = recordFilter;
+    }
+
+    @Override
+    public final boolean isValid(T record) {
+        return recordFilter.isValid(recordMapper.map(record));
+    }
+
+}
