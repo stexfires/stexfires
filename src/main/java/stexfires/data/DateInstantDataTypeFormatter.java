9950920c68076b84cdf426a9266089690d178ba1
mkalb <mkalb@mkalb.de>
2022-12-24 14:46:20 +0100

    Add DateInstantDataTypeFormatter and DateInstantDataTypeParser

---
+++ src/main/java/stexfires/data/DateInstantDataTypeFormatter.java
@@ -0,0 +1,45 @@
+package stexfires.data;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.time.DateTimeException;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+import java.util.function.Supplier;
+
+/**
+ * @author Mathias Kalb
+ * @since 0.1
+ */
+public final class DateInstantDataTypeFormatter implements DataTypeFormatter<Instant> {
+
+    private final DateTimeFormatter dateTimeFormatter;
+    private final Supplier<String> nullSourceSupplier;
+
+    public DateInstantDataTypeFormatter(@NotNull DateTimeFormatter dateTimeFormatter,
+                                        @Nullable Supplier<String> nullSourceSupplier) {
+        Objects.requireNonNull(dateTimeFormatter);
+        this.dateTimeFormatter = dateTimeFormatter;
+        this.nullSourceSupplier = nullSourceSupplier;
+    }
+
+    @Override
+    public String format(Instant source) throws DataTypeFormatException {
+        if (source == null) {
+            if (nullSourceSupplier == null) {
+                throw new DataTypeFormatException("Source is null.");
+            } else {
+                return nullSourceSupplier.get();
+            }
+        } else {
+            try {
+                return dateTimeFormatter.format(source);
+            } catch (DateTimeException e) {
+                throw new DataTypeFormatException(e.getMessage());
+            }
+        }
+    }
+
+}
---
+++ src/main/java/stexfires/data/DateInstantDataTypeParser.java
@@ -0,0 +1,54 @@
+package stexfires.data;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Objects;
+import java.util.function.Supplier;
+
+/**
+ * @author Mathias Kalb
+ * @since 0.1
+ */
+public final class DateInstantDataTypeParser implements DataTypeParser<Instant> {
+
+    private final DateTimeFormatter dateTimeFormatter;
+    private final Supplier<Instant> nullSourceSupplier;
+    private final Supplier<Instant> emptySourceSupplier;
+
+    public DateInstantDataTypeParser(@NotNull DateTimeFormatter dateTimeFormatter,
+                                     @Nullable Supplier<Instant> nullSourceSupplier,
+                                     @Nullable Supplier<Instant> emptySourceSupplier) {
+        Objects.requireNonNull(dateTimeFormatter);
+        this.dateTimeFormatter = dateTimeFormatter;
+        this.nullSourceSupplier = nullSourceSupplier;
+        this.emptySourceSupplier = emptySourceSupplier;
+    }
+
+    @Override
+    public Instant parse(String source) throws DataTypeParseException {
+        if (source == null) {
+            if (nullSourceSupplier == null) {
+                throw new DataTypeParseException("Source is null.");
+            } else {
+                return nullSourceSupplier.get();
+            }
+        } else if (source.isEmpty()) {
+            if (emptySourceSupplier == null) {
+                throw new DataTypeParseException("Source is empty.");
+            } else {
+                return emptySourceSupplier.get();
+            }
+        } else {
+            try {
+                return dateTimeFormatter.parse(source, Instant::from);
+            } catch (DateTimeParseException e) {
+                throw new DataTypeParseException(e.getMessage());
+            }
+        }
+    }
+
+}
