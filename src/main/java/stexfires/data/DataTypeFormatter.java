fd6effe926538efa8468553b767b801fe721bb3a
mkalb <mkalb@mkalb.de>
2022-12-26 14:52:31 +0100

    Add static and default methods to DataType interfaces

--- src/main/java/stexfires/data/DataTypeFormatter.java
+++ src/main/java/stexfires/data/DataTypeFormatter.java
@@ -2,7 +2,10 @@ package stexfires.data;
 
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 /**
  * @author Mathias Kalb
@@ -11,10 +14,24 @@ import java.util.Optional;
 @FunctionalInterface
 public interface DataTypeFormatter<T> {
 
+    static <T> DataTypeFormatter<T> ofFunction(Function<T, String> function) {
+        Objects.requireNonNull(function);
+        return function::apply;
+    }
+
     @Nullable String format(@Nullable T source) throws DataTypeFormatException;
 
     default @Nullable Optional<String> formatToOptional(@Nullable T source) throws DataTypeFormatException {
         return Optional.ofNullable(format(source));
+    }
+
+    default Function<T, String> asFunction() {
+        return this::format;
+    }
+
+    default DataTypeFormatter<T> andThen(UnaryOperator<String> stringUnaryOperator) {
+        Objects.requireNonNull(stringUnaryOperator);
+        return source -> stringUnaryOperator.apply(format(source));
     }
 
 }
--- src/main/java/stexfires/data/DataTypeParser.java
+++ src/main/java/stexfires/data/DataTypeParser.java
@@ -3,7 +3,10 @@ package stexfires.data;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 /**
  * @author Mathias Kalb
@@ -12,10 +15,24 @@ import java.util.Optional;
 @FunctionalInterface
 public interface DataTypeParser<T> {
 
+    static <T> DataTypeParser<T> ofFunction(Function<String, T> function) {
+        Objects.requireNonNull(function);
+        return function::apply;
+    }
+
     @Nullable T parse(@Nullable String source) throws DataTypeParseException;
 
     default @NotNull Optional<T> parseToOptional(@Nullable String source) throws DataTypeParseException {
         return Optional.ofNullable(parse(source));
+    }
+
+    default Function<String, T> asFunction() {
+        return this::parse;
+    }
+
+    default DataTypeParser<T> andThen(UnaryOperator<T> dataTypeUnaryOperator) {
+        Objects.requireNonNull(dataTypeUnaryOperator);
+        return source -> dataTypeUnaryOperator.apply(parse(source));
     }
 
 }
