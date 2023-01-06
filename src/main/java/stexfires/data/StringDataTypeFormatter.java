00313730ac1a0813f6be471e3da8cae29633c4cf
mkalb <mkalb@mkalb.de>
2023-01-06 20:56:26 +0100

    Removed 'emptySourceSupplier' from StringDataTypeFormatter

--- src/main/java/stexfires/data/StringDataTypeFormatter.java
+++ src/main/java/stexfires/data/StringDataTypeFormatter.java
@@ -2,7 +2,6 @@ package stexfires.data;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import stexfires.util.Strings;
 import stexfires.util.function.Suppliers;
 
 import java.util.Objects;
@@ -17,41 +16,30 @@ public final class StringDataTypeFormatter implements DataTypeFormatter<String> 
 
     private final UnaryOperator<String> formatter;
     private final Supplier<String> nullSourceSupplier;
-    private final Supplier<String> emptySourceSupplier;
 
     public StringDataTypeFormatter(@Nullable UnaryOperator<String> formatter,
-                                   @Nullable Supplier<String> nullSourceSupplier,
-                                   @Nullable Supplier<String> emptySourceSupplier) {
+                                   @Nullable Supplier<String> nullSourceSupplier) {
         this.formatter = formatter;
         this.nullSourceSupplier = nullSourceSupplier;
-        this.emptySourceSupplier = emptySourceSupplier;
     }
 
     public static StringDataTypeFormatter newFormatter(@NotNull UnaryOperator<String> formatter) {
         Objects.requireNonNull(formatter);
         return new StringDataTypeFormatter(
                 formatter,
-                Suppliers.constantNull(),
-                Suppliers.constant(Strings.EMPTY));
+                Suppliers.constantNull());
     }
 
     public static StringDataTypeFormatter newIdentityFormatter() {
         return new StringDataTypeFormatter(
                 null,
-                Suppliers.constantNull(),
-                Suppliers.constant(Strings.EMPTY));
+                Suppliers.constantNull());
     }
 
     @Override
     public @Nullable String format(@Nullable String source) throws DataTypeFormatException {
         if (source == null) {
             return handleNullSource(nullSourceSupplier);
-        } else if (source.isEmpty()) {
-            if (emptySourceSupplier == null) {
-                throw new DataTypeFormatException("Source is empty.");
-            } else {
-                return emptySourceSupplier.get();
-            }
         } else {
             if (formatter != null) {
                 return formatter.apply(source);
