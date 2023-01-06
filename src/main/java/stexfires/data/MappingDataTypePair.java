cc5f8dbf53445283007bcefe3887c61d7cec8c5e
mkalb <mkalb@mkalb.de>
2023-01-06 22:43:55 +0100

    Add static method 'createPairListCodingErrorAction'

--- src/main/java/stexfires/data/MappingDataTypePair.java
+++ src/main/java/stexfires/data/MappingDataTypePair.java
@@ -2,6 +2,7 @@ package stexfires.data;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.nio.charset.CodingErrorAction;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
@@ -20,6 +21,12 @@ public record MappingDataTypePair<T>(@NotNull T dataValue,
         Objects.requireNonNull(mapper);
         return dataValueStream.map(dataValue -> new MappingDataTypePair<>(dataValue, mapper.apply(dataValue)))
                               .toList();
+    }
+
+    public static List<MappingDataTypePair<CodingErrorAction>> createPairListCodingErrorAction() {
+        return MappingDataTypePair.createPairList(Stream.of(
+                CodingErrorAction.IGNORE, CodingErrorAction.REPLACE, CodingErrorAction.REPORT
+        ), CodingErrorAction::toString);
     }
 
 }
