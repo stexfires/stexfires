c874b719cd9ac9db6d2b92e2326df1f813206ec2
mkalb <mkalb@mkalb.de>
2023-07-03 21:27:47 +0200

    Add empty StringComparatorsTest

---
+++ stexfires-util/src/test/java/stexfires/util/StringComparatorsTest.java
@@ -0,0 +1,108 @@
+package stexfires.util;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for {@link StringComparators}.
+ */
+class StringComparatorsTest {
+
+    /**
+     * Test method for {@link StringComparators#compareTo()}.
+     */
+    @Test
+    void compareTo() {
+    }
+
+    /**
+     * Test method for {@link StringComparators#compareToIgnoreCase()}.
+     */
+    @Test
+    void compareToIgnoreCase() {
+    }
+
+    /**
+     * Test method for {@link StringComparators#collatorComparator(java.text.Collator)}.
+     */
+    @Test
+    void collatorComparator_Collator() {
+    }
+
+    /**
+     * Test method for {@link StringComparators#collatorComparator(java.util.Locale)}.
+     */
+    @Test
+    void collatorComparator_Locale() {
+    }
+
+    /**
+     * Test method for {@link StringComparators#collatorComparator(java.util.Locale, int)}.
+     */
+    @Test
+    void collatorComparator_Locale_Strength() {
+    }
+
+    /**
+     * Test method for {@link StringComparators#normalizedComparator(java.util.function.UnaryOperator, java.util.Comparator)}.
+     */
+    @Test
+    void normalizedComparator() {
+    }
+
+    /**
+     * Test method for {@link StringComparators#lengthComparator()}.
+     */
+    @Test
+    void lengthComparator() {
+    }
+
+    /**
+     * Test method for {@link StringComparators#primitiveIntComparator(int, int)}
+     */
+    @Test
+    void primitiveIntComparator() {
+    }
+
+    /**
+     * Test method for {@link StringComparators#primitiveLongComparator(long, long)}
+     */
+    @Test
+    void primitiveLongComparator() {
+    }
+
+    /**
+     * Test method for {@link StringComparators#primitiveDoubleComparator(double, double)}
+     */
+    @Test
+    void primitiveDoubleComparator() {
+    }
+
+    /**
+     * Test method for {@link StringComparators#integerComparator(Integer, Integer, java.util.Comparator)}
+     */
+    @Test
+    void integerComparator() {
+    }
+
+    /**
+     * Test method for {@link StringComparators#longComparator(Long, Long, java.util.Comparator)}
+     */
+    @Test
+    void longComparator() {
+    }
+
+    /**
+     * Test method for {@link StringComparators#doubleComparator(Double, Double, java.util.Comparator)}
+     */
+    @Test
+    void doubleComparator() {
+    }
+
+    /**
+     * Test method for {@link StringComparators#extractorComparator(java.util.function.Function, java.util.Comparator)}
+     */
+    @Test
+    void extractorComparator() {
+    }
+
+}
