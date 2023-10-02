d5bd295b4536b273531b922fde4d8e0df83d4b18
mkalb <mkalb@mkalb.de>
2023-10-02 20:53:24 +0200

    Add export statements to "stexfires.examples"

--- stexfires-examples/src/main/java/module-info.java
+++ stexfires-examples/src/main/java/module-info.java
@@ -4,4 +4,10 @@ module stexfires.examples {
     requires stexfires.io;
     requires stexfires.record;
     requires stexfires.util;
+    exports stexfires.examples.data;
+    exports stexfires.examples.io;
+    exports stexfires.examples.javatest;
+    exports stexfires.examples.record;
+    exports stexfires.examples.util;
+    exports stexfires.examples.wiki;
 }
