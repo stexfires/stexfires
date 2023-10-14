b8b937278adbdd3b2d682be977fb02a95ed447ce
mkalb <mkalb@mkalb.de>
2023-10-14 15:14:23 +0200

    Add methods from interface CharSequence to LineSeparator

--- stexfires-util/src/main/java/stexfires/util/LineSeparator.java
+++ stexfires-util/src/main/java/stexfires/util/LineSeparator.java
@@ -4,7 +4,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.nio.charset.Charset;
-import java.util.Arrays;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Supplier;
@@ -13,11 +12,14 @@ import java.util.stream.Stream;
 
 /**
  * Enum with three common line separators for text files: {@code LF, CR, CR_LF}.
+ * <p>
+ * The methods {@code string()} and {@code toString()} return the line separator as a {@code String} with one or two characters.
+ * All methods of the {@code CharSequence} interface are based on this {@code String} representation.
  *
  * @since 0.1
  */
 @SuppressWarnings("HardcodedLineSeparator")
-public enum LineSeparator {
+public enum LineSeparator implements CharSequence {
 
     /**
      * Line feed: {@code '\n'} (0xA)
@@ -34,7 +36,7 @@ public enum LineSeparator {
      */
     CR_LF("\r\n");
 
-    private final String separator;
+    private final @NotNull String separator;
 
     /**
      * Constructs a {@code LineSeparator} with the specified line separator string.
@@ -42,7 +44,7 @@ public enum LineSeparator {
      * @param separator line separator as a {@code String} with one or two characters
      * @see LineSeparator#string()
      */
-    LineSeparator(String separator) {
+    LineSeparator(@NotNull String separator) {
         this.separator = separator;
     }
 
@@ -53,23 +55,29 @@ public enum LineSeparator {
      * @return system line separator
      * @see System#lineSeparator()
      */
-    public static LineSeparator systemLineSeparator() {
-        return Arrays.stream(LineSeparator.values())
-                     .filter(lineSeparator -> lineSeparator.string().equals(System.lineSeparator()))
-                     .findFirst()
-                     .orElse(LF);
+    @SuppressWarnings("DuplicateBranchesInSwitch")
+    public static @NotNull LineSeparator systemLineSeparator() {
+        return switch (System.lineSeparator()) {
+            case "\n" -> LF;
+            case "\r" -> CR;
+            case "\r\n" -> CR_LF;
+            default -> LF;
+        };
     }
 
     /**
      * Returns the {@code LineSeparator} matching the passed {@code String}.
      *
-     * @param string Optional {@code String} for lookup. Can be {@code null}.
+     * @param lookupString Optional {@code String} for lookup. Can be {@code null}.
      * @return {@code LineSeparator} as {@code Optional} matching the passed {@code String}
      */
-    public static Optional<LineSeparator> lookup(@Nullable String string) {
-        return Arrays.stream(LineSeparator.values())
-                     .filter(lineSeparator -> lineSeparator.string().equals(string))
-                     .findFirst();
+    public static @NotNull Optional<LineSeparator> lookup(@Nullable String lookupString) {
+        return switch (lookupString) {
+            case "\n" -> Optional.of(LF);
+            case "\r" -> Optional.of(CR);
+            case "\r\n" -> Optional.of(CR_LF);
+            case null, default -> Optional.empty();
+        };
     }
 
     /**
@@ -77,7 +85,7 @@ public enum LineSeparator {
      *
      * @return {@code LineSeparator} as a {@code String} with one or two characters
      */
-    public final String string() {
+    public final @NotNull String string() {
         return separator;
     }
 
@@ -90,19 +98,72 @@ public enum LineSeparator {
      * @see String#repeat(int)
      * @see LineSeparator#string()
      */
-    public final String repeat(int count) {
+    public final @NotNull String repeat(int count) {
         return separator.repeat(count);
     }
 
     /**
      * Returns the {@code String} length.
      *
-     * @return {@code String} length
+     * @return {@code String} length, always 1 or 2
      * @see String#length()
      * @see LineSeparator#string()
      */
+    @Override
     public final int length() {
-        return separator.length();
+        return switch (this) {
+            case LF, CR -> 1;
+            case CR_LF -> 2;
+        };
+    }
+
+    /**
+     * Returns the {@code char} value at the specified index.
+     *
+     * @param index the index of the {@code char} value to be returned
+     * @return the specified {@code char} value
+     * @throws IndexOutOfBoundsException if the {@code index} argument is negative or not less than
+     *                                   {@code length()}
+     * @see String#charAt(int)
+     * @see LineSeparator#string()
+     */
+    @Override
+    public final char charAt(int index) {
+        return separator.charAt(index);
+    }
+
+    /**
+     * Returns always {@code false}.
+     *
+     * @return always {@code false}
+     * @see String#isEmpty()
+     * @see LineSeparator#string()
+     */
+    @Override
+    public final boolean isEmpty() {
+        return false;
+    }
+
+    /**
+     * Returns a {@code CharSequence} that is a subsequence of this sequence.
+     * The subsequence starts with the {@code char} value at the specified index and
+     * ends with the {@code char} value at index {@code end - 1}.  The length
+     * (in {@code char}s) of the
+     * returned sequence is {@code end - start}, so if {@code start == end}
+     * then an empty sequence is returned.
+     *
+     * @param start the start index, inclusive
+     * @param end   the end index, exclusive
+     * @return the specified subsequence
+     * @throws IndexOutOfBoundsException if {@code start} or {@code end} are negative,
+     *                                   if {@code end} is greater than {@code length()},
+     *                                   or if {@code start} is greater than {@code end}
+     * @see String#subSequence(int, int)
+     * @see LineSeparator#string()
+     */
+    @Override
+    public final @NotNull CharSequence subSequence(int start, int end) {
+        return separator.subSequence(start, end);
     }
 
     /**
@@ -112,7 +173,7 @@ public enum LineSeparator {
      * @see java.util.function.Supplier
      * @see LineSeparator#string()
      */
-    public final Supplier<String> supplier() {
+    public final @NotNull Supplier<String> supplier() {
         return () -> separator;
     }
 
@@ -123,7 +184,7 @@ public enum LineSeparator {
      * @see java.util.stream.Stream#of(Object)
      * @see LineSeparator#string()
      */
-    public final Stream<String> stream() {
+    public final @NotNull Stream<String> stream() {
         return Stream.of(separator);
     }
 
@@ -134,8 +195,14 @@ public enum LineSeparator {
      * @see String#chars()
      * @see LineSeparator#string()
      */
-    public final IntStream chars() {
-        return separator.chars();
+    @SuppressWarnings("MagicNumber")
+    @Override
+    public final @NotNull IntStream chars() {
+        return switch (this) {
+            case LF -> IntStream.of(10);
+            case CR -> IntStream.of(13);
+            case CR_LF -> IntStream.of(13, 10);
+        };
     }
 
     /**
@@ -145,8 +212,14 @@ public enum LineSeparator {
      * @see String#codePoints()
      * @see LineSeparator#string()
      */
-    public final IntStream codePoints() {
-        return separator.codePoints();
+    @SuppressWarnings("MagicNumber")
+    @Override
+    public final @NotNull IntStream codePoints() {
+        return switch (this) {
+            case LF -> IntStream.of(10);
+            case CR -> IntStream.of(13);
+            case CR_LF -> IntStream.of(13, 10);
+        };
     }
 
     /**
@@ -174,7 +247,7 @@ public enum LineSeparator {
      * @return {@code LineSeparator} as a regular expression
      * @see java.util.regex.Pattern
      */
-    public final String regex() {
+    public final @NotNull String regex() {
         return switch (this) {
             case LF -> "\\n";
             case CR -> "\\r";
@@ -193,7 +266,7 @@ public enum LineSeparator {
      *
      * @return {@code LineSeparator} as an escaped Java string
      */
-    public final String escapedJavaString() {
+    public final @NotNull String escapedJavaString() {
         return switch (this) {
             case LF -> "\\n";
             case CR -> "\\r";
@@ -208,7 +281,7 @@ public enum LineSeparator {
      * @see LineSeparator#string()
      */
     @Override
-    public final String toString() {
+    public final @NotNull String toString() {
         return separator;
     }
 
--- stexfires-util/src/test/java/stexfires/util/LineSeparatorTest.java
+++ stexfires-util/src/test/java/stexfires/util/LineSeparatorTest.java
@@ -10,6 +10,7 @@ import java.util.function.Supplier;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static stexfires.util.LineSeparator.CR;
 import static stexfires.util.LineSeparator.CR_LF;
@@ -18,7 +19,7 @@ import static stexfires.util.LineSeparator.LF;
 /**
  * Tests for {@link LineSeparator}.
  */
-@SuppressWarnings("HardcodedLineSeparator")
+@SuppressWarnings({"HardcodedLineSeparator", "MagicNumber"})
 class LineSeparatorTest {
 
     private static final String STRING_LF = "\n";
@@ -43,6 +44,29 @@ class LineSeparatorTest {
         assertEquals(0, LF.ordinal());
         assertEquals(1, CR.ordinal());
         assertEquals(2, CR_LF.ordinal());
+    }
+
+    /**
+     * Test method for {@link LineSeparator#systemLineSeparator()}.
+     */
+    @Test
+    void systemLineSeparator() {
+        assertEquals(System.lineSeparator(), LineSeparator.systemLineSeparator().string());
+    }
+
+    /**
+     * Test method for {@link LineSeparator#lookup(String)}.
+     */
+    @Test
+    void lookup() {
+        assertEquals(Optional.of(LF), LineSeparator.lookup(STRING_LF));
+        assertEquals(Optional.of(CR), LineSeparator.lookup(STRING_CR));
+        assertEquals(Optional.of(CR_LF), LineSeparator.lookup(STRING_CR_LF));
+        assertEquals(Optional.empty(), LineSeparator.lookup(""));
+        assertEquals(Optional.empty(), LineSeparator.lookup("  "));
+        assertEquals(Optional.empty(), LineSeparator.lookup("unknown"));
+        assertEquals(Optional.empty(), LineSeparator.lookup("\t"));
+        assertEquals(Optional.empty(), LineSeparator.lookup(null));
     }
 
     /**
@@ -76,6 +100,10 @@ class LineSeparatorTest {
         assertEquals(STRING_CR + STRING_CR + STRING_CR, CR.repeat(3));
         assertEquals(STRING_CR_LF + STRING_CR_LF + STRING_CR_LF, CR_LF.repeat(3));
 
+        assertEquals(100, LF.repeat(100).length());
+        assertEquals(100, CR.repeat(100).length());
+        assertEquals(200, CR_LF.repeat(100).length());
+
         assertThrows(IllegalArgumentException.class, () -> LF.repeat(-1));
         assertThrows(IllegalArgumentException.class, () -> CR.repeat(-1));
         assertThrows(IllegalArgumentException.class, () -> CR_LF.repeat(-1));
@@ -86,9 +114,80 @@ class LineSeparatorTest {
      */
     @Test
     void length() {
+        assertEquals(STRING_LF.length(), LF.length());
+        assertEquals(STRING_CR.length(), CR.length());
+        assertEquals(STRING_CR_LF.length(), CR_LF.length());
+
         assertEquals(1, LF.length());
         assertEquals(1, CR.length());
         assertEquals(2, CR_LF.length());
+    }
+
+    /**
+     * Test method for {@link LineSeparator#charAt(int)}.
+     */
+    @Test
+    void charAt() {
+        assertEquals(STRING_LF.charAt(0), LF.charAt(0));
+        assertEquals(STRING_CR.charAt(0), CR.charAt(0));
+        assertEquals(STRING_CR_LF.charAt(0), CR_LF.charAt(0));
+        assertEquals(STRING_CR_LF.charAt(1), CR_LF.charAt(1));
+
+        assertEquals('\n', LF.charAt(0));
+        assertEquals('\r', CR.charAt(0));
+        assertEquals('\r', CR_LF.charAt(0));
+        assertEquals('\n', CR_LF.charAt(1));
+
+        assertThrows(IndexOutOfBoundsException.class, () -> LF.charAt(-1));
+        assertThrows(IndexOutOfBoundsException.class, () -> CR.charAt(-1));
+        assertThrows(IndexOutOfBoundsException.class, () -> CR_LF.charAt(-1));
+        assertThrows(IndexOutOfBoundsException.class, () -> LF.charAt(1));
+        assertThrows(IndexOutOfBoundsException.class, () -> CR.charAt(1));
+        assertThrows(IndexOutOfBoundsException.class, () -> CR_LF.charAt(2));
+    }
+
+    /**
+     * Test method for {@link LineSeparator#isEmpty()}.
+     */
+    @SuppressWarnings("ConstantValue")
+    @Test
+    void isEmpty() {
+        assertEquals(STRING_LF.isEmpty(), LF.isEmpty());
+        assertEquals(STRING_CR.isEmpty(), CR.isEmpty());
+        assertEquals(STRING_CR_LF.isEmpty(), CR_LF.isEmpty());
+
+        assertFalse(LF.isEmpty());
+        assertFalse(CR.isEmpty());
+        assertFalse(CR_LF.isEmpty());
+    }
+
+    /**
+     * Test method for {@link LineSeparator#subSequence(int, int)}.
+     */
+    @Test
+    void subSequence() {
+        assertEquals(STRING_LF.subSequence(0, 1), LF.subSequence(0, 1));
+        assertEquals(STRING_CR.subSequence(0, 1), CR.subSequence(0, 1));
+        assertEquals(STRING_CR_LF.subSequence(0, 1), CR_LF.subSequence(0, 1));
+        assertEquals(STRING_CR_LF.subSequence(0, 2), CR_LF.subSequence(0, 2));
+        assertEquals(STRING_CR_LF.subSequence(1, 2), CR_LF.subSequence(1, 2));
+
+        assertEquals("\n", LF.subSequence(0, 1));
+        assertEquals("\r", CR.subSequence(0, 1));
+        assertEquals("\r", CR_LF.subSequence(0, 1));
+        assertEquals("\r\n", CR_LF.subSequence(0, 2));
+        assertEquals("\n", CR_LF.subSequence(1, 2));
+
+        assertThrows(IndexOutOfBoundsException.class, () -> LF.subSequence(-1, 0));
+        assertThrows(IndexOutOfBoundsException.class, () -> CR.subSequence(-1, 0));
+        assertThrows(IndexOutOfBoundsException.class, () -> CR_LF.subSequence(-1, 0));
+
+        assertThrows(IndexOutOfBoundsException.class, () -> LF.subSequence(1, 0));
+        assertThrows(IndexOutOfBoundsException.class, () -> CR.subSequence(1, 0));
+        assertThrows(IndexOutOfBoundsException.class, () -> CR_LF.subSequence(1, 0));
+
+        assertThrows(IndexOutOfBoundsException.class, () -> LF.subSequence(1, 2));
+        assertThrows(IndexOutOfBoundsException.class, () -> CR.subSequence(1, 2));
     }
 
     /**
@@ -100,21 +199,25 @@ class LineSeparatorTest {
         assertEquals(STRING_LF, supplierLF.get());
         assertEquals(STRING_LF, supplierLF.get());
         assertEquals(STRING_LF, supplierLF.get());
+        assertNotSame(supplierLF, LF.supplier());
 
         Supplier<String> supplierCR = CR.supplier();
         assertEquals(STRING_CR, supplierCR.get());
         assertEquals(STRING_CR, supplierCR.get());
         assertEquals(STRING_CR, supplierCR.get());
+        assertNotSame(supplierCR, CR.supplier());
 
         Supplier<String> supplierCRLF = CR_LF.supplier();
         assertEquals(STRING_CR_LF, supplierCRLF.get());
         assertEquals(STRING_CR_LF, supplierCRLF.get());
         assertEquals(STRING_CR_LF, supplierCRLF.get());
+        assertNotSame(supplierCRLF, CR_LF.supplier());
     }
 
     /**
      * Test method for {@link LineSeparator#stream()}.
      */
+    @SuppressWarnings("EqualsWithItself")
     @Test
     void stream() {
         assertEquals(1, LF.stream().count());
@@ -128,6 +231,10 @@ class LineSeparatorTest {
         assertFalse(LF.stream().isParallel());
         assertFalse(CR.stream().isParallel());
         assertFalse(CR_LF.stream().isParallel());
+
+        assertNotSame(LF.stream(), LF.stream());
+        assertNotSame(CR.stream(), CR.stream());
+        assertNotSame(CR_LF.stream(), CR_LF.stream());
     }
 
     /**
@@ -147,6 +254,10 @@ class LineSeparatorTest {
         assertFalse(LF.chars().isParallel());
         assertFalse(CR.chars().isParallel());
         assertFalse(CR_LF.chars().isParallel());
+
+        assertNotSame(LF.chars(), LF.chars());
+        assertNotSame(CR.chars(), CR.chars());
+        assertNotSame(CR_LF.chars(), CR_LF.chars());
     }
 
     /**
@@ -166,6 +277,10 @@ class LineSeparatorTest {
         assertFalse(LF.codePoints().isParallel());
         assertFalse(CR.codePoints().isParallel());
         assertFalse(CR_LF.codePoints().isParallel());
+
+        assertNotSame(LF.codePoints(), LF.codePoints());
+        assertNotSame(CR.codePoints(), CR.codePoints());
+        assertNotSame(CR_LF.codePoints(), CR_LF.codePoints());
     }
 
     /**
@@ -174,6 +289,10 @@ class LineSeparatorTest {
     @SuppressWarnings("MagicNumber")
     @Test
     void bytes() {
+        assertArrayEquals(STRING_LF.getBytes(StandardCharsets.US_ASCII), LF.bytes(StandardCharsets.US_ASCII));
+        assertArrayEquals(STRING_CR.getBytes(StandardCharsets.US_ASCII), CR.bytes(StandardCharsets.US_ASCII));
+        assertArrayEquals(STRING_CR_LF.getBytes(StandardCharsets.US_ASCII), CR_LF.bytes(StandardCharsets.US_ASCII));
+
         assertArrayEquals(new byte[]{0x0A}, LF.bytes(StandardCharsets.US_ASCII));
         assertArrayEquals(new byte[]{0x0D}, CR.bytes(StandardCharsets.US_ASCII));
         assertArrayEquals(new byte[]{0x0D, 0x0A}, CR_LF.bytes(StandardCharsets.US_ASCII));
@@ -181,6 +300,14 @@ class LineSeparatorTest {
         assertArrayEquals(new byte[]{0x0A}, LF.bytes(StandardCharsets.UTF_8));
         assertArrayEquals(new byte[]{0x0D}, CR.bytes(StandardCharsets.UTF_8));
         assertArrayEquals(new byte[]{0x0D, 0x0A}, CR_LF.bytes(StandardCharsets.UTF_8));
+
+        assertNotSame(LF.bytes(StandardCharsets.US_ASCII), LF.bytes(StandardCharsets.US_ASCII));
+        assertNotSame(CR.bytes(StandardCharsets.US_ASCII), CR.bytes(StandardCharsets.US_ASCII));
+        assertNotSame(CR_LF.bytes(StandardCharsets.US_ASCII), CR_LF.bytes(StandardCharsets.US_ASCII));
+
+        assertArrayEquals(LF.bytes(StandardCharsets.US_ASCII), LF.bytes(StandardCharsets.US_ASCII));
+        assertArrayEquals(CR.bytes(StandardCharsets.US_ASCII), CR.bytes(StandardCharsets.US_ASCII));
+        assertArrayEquals(CR_LF.bytes(StandardCharsets.US_ASCII), CR_LF.bytes(StandardCharsets.US_ASCII));
     }
 
     /**
@@ -211,26 +338,6 @@ class LineSeparatorTest {
         assertEquals(STRING_LF, LF.toString());
         assertEquals(STRING_CR, CR.toString());
         assertEquals(STRING_CR_LF, CR_LF.toString());
-    }
-
-    /**
-     * Test method for {@link LineSeparator#systemLineSeparator()}.
-     */
-    @Test
-    void systemLineSeparator() {
-        assertEquals(System.lineSeparator(), LineSeparator.systemLineSeparator().string());
-    }
-
-    /**
-     * Test method for {@link LineSeparator#lookup(String)}.
-     */
-    @Test
-    void lookup() {
-        assertEquals(Optional.of(LF), LineSeparator.lookup(STRING_LF));
-        assertEquals(Optional.of(CR), LineSeparator.lookup(STRING_CR));
-        assertEquals(Optional.of(CR_LF), LineSeparator.lookup(STRING_CR_LF));
-        assertEquals(Optional.empty(), LineSeparator.lookup(""));
-        assertEquals(Optional.empty(), LineSeparator.lookup(null));
     }
 
 }
