85e8b41a2cfbf8bb36025b1a72fe8eff0c386f36
mkalb <mkalb@mkalb.de>
2023-12-27 08:27:35 +0100

    Optimize and enhance NumberToStringFunctions

--- stexfires-util/src/main/java/stexfires/util/function/NumberToStringFunctions.java
+++ stexfires-util/src/main/java/stexfires/util/function/NumberToStringFunctions.java
@@ -1,5 +1,7 @@
 package stexfires.util.function;
 
+import org.jspecify.annotations.Nullable;
+
 import java.math.BigInteger;
 import java.text.NumberFormat;
 import java.util.HexFormat;
@@ -7,7 +9,10 @@ import java.util.Locale;
 import java.util.Objects;
 import java.util.function.Function;
 import java.util.function.IntFunction;
+import java.util.function.IntPredicate;
 import java.util.function.LongFunction;
+import java.util.function.LongPredicate;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 /**
@@ -21,16 +26,88 @@ import java.util.function.Supplier;
  */
 public final class NumberToStringFunctions {
 
-    private static final int RADIX_BINARY = 2;
-    private static final int RADIX_OCTAL = 8;
-    private static final int RADIX_HEX = 16;
+    public static final int RADIX_BINARY = 2;
+    public static final int RADIX_OCTAL = 8;
+    public static final int RADIX_HEX = 16;
 
     private NumberToStringFunctions() {
+    }
+
+    public static <T extends Number> Function<@Nullable T, String> constant(String constant) {
+        return n -> constant;
+    }
+
+    public static <T extends Number> Function<@Nullable T, String> supplier(Supplier<String> supplier) {
+        Objects.requireNonNull(supplier);
+        return n -> supplier.get();
+    }
+
+    public static <T extends Number> Function<@Nullable T, String> nullHandler(String nullValue,
+                                                                               Function<T, String> nonNullFunction) {
+        Objects.requireNonNull(nullValue);
+        Objects.requireNonNull(nonNullFunction);
+        return n -> (n == null) ? nullValue : nonNullFunction.apply(n);
+    }
+
+    public static <T extends Number> Function<T, String> conditional(Predicate<T> condition,
+                                                                     Function<T, String> trueFunction,
+                                                                     Function<T, String> falseFunction) {
+        Objects.requireNonNull(condition);
+        Objects.requireNonNull(trueFunction);
+        Objects.requireNonNull(falseFunction);
+        return n -> condition.test(n) ? trueFunction.apply(n) : falseFunction.apply(n);
+    }
+
+    public static <T extends Number> Function<T, String> formatted(Locale locale, String format) {
+        Objects.requireNonNull(locale);
+        Objects.requireNonNull(format);
+        return n -> String.format(locale, format, n);
+    }
+
+    public static <T extends Number> Function<T, String> numberFormat(NumberFormat numberFormat) {
+        Objects.requireNonNull(numberFormat);
+        return numberFormat::format;
     }
 
     public static final class PrimitiveIntToStringFunctions {
 
         private PrimitiveIntToStringFunctions() {
+        }
+
+        public static IntFunction<String> constant(String constant) {
+            return n -> constant;
+        }
+
+        public static IntFunction<String> supplier(Supplier<String> supplier) {
+            Objects.requireNonNull(supplier);
+            return n -> supplier.get();
+        }
+
+        public static Function<@Nullable Integer, String> nullHandler(String nullValue,
+                                                                      IntFunction<String> nonNullFunction) {
+            Objects.requireNonNull(nullValue);
+            Objects.requireNonNull(nonNullFunction);
+            return n -> (n == null) ? nullValue : nonNullFunction.apply(n);
+        }
+
+        public static IntFunction<String> conditional(IntPredicate condition,
+                                                      IntFunction<String> trueFunction,
+                                                      IntFunction<String> falseFunction) {
+            Objects.requireNonNull(condition);
+            Objects.requireNonNull(trueFunction);
+            Objects.requireNonNull(falseFunction);
+            return n -> condition.test(n) ? trueFunction.apply(n) : falseFunction.apply(n);
+        }
+
+        public static IntFunction<String> formatted(Locale locale, String format) {
+            Objects.requireNonNull(locale);
+            Objects.requireNonNull(format);
+            return n -> String.format(locale, format, n);
+        }
+
+        public static IntFunction<String> numberFormat(NumberFormat numberFormat) {
+            Objects.requireNonNull(numberFormat);
+            return numberFormat::format;
         }
 
         public static IntFunction<String> binary() {
@@ -57,31 +134,47 @@ public final class NumberToStringFunctions {
             return Integer::toOctalString;
         }
 
-        public static IntFunction<String> formatted(Locale locale, String format) {
-            Objects.requireNonNull(locale);
-            Objects.requireNonNull(format);
-            return n -> String.format(locale, format, n);
-        }
-
-        public static IntFunction<String> numberFormat(NumberFormat numberFormat) {
-            Objects.requireNonNull(numberFormat);
-            return numberFormat::format;
-        }
-
-        public static IntFunction<String> constant(String constant) {
-            return n -> constant;
-        }
-
-        public static IntFunction<String> supplier(Supplier<String> supplier) {
-            Objects.requireNonNull(supplier);
-            return n -> supplier.get();
-        }
-
     }
 
     public static final class PrimitiveLongToStringFunctions {
 
         private PrimitiveLongToStringFunctions() {
+        }
+
+        public static LongFunction<String> constant(String constant) {
+            return n -> constant;
+        }
+
+        public static LongFunction<String> supplier(Supplier<String> supplier) {
+            Objects.requireNonNull(supplier);
+            return n -> supplier.get();
+        }
+
+        public static Function<@Nullable Long, String> nullHandler(String nullValue,
+                                                                   LongFunction<String> nonNullFunction) {
+            Objects.requireNonNull(nullValue);
+            Objects.requireNonNull(nonNullFunction);
+            return n -> (n == null) ? nullValue : nonNullFunction.apply(n);
+        }
+
+        public static LongFunction<String> conditional(LongPredicate condition,
+                                                       LongFunction<String> trueFunction,
+                                                       LongFunction<String> falseFunction) {
+            Objects.requireNonNull(condition);
+            Objects.requireNonNull(trueFunction);
+            Objects.requireNonNull(falseFunction);
+            return n -> condition.test(n) ? trueFunction.apply(n) : falseFunction.apply(n);
+        }
+
+        public static LongFunction<String> formatted(Locale locale, String format) {
+            Objects.requireNonNull(locale);
+            Objects.requireNonNull(format);
+            return n -> String.format(locale, format, n);
+        }
+
+        public static LongFunction<String> numberFormat(NumberFormat numberFormat) {
+            Objects.requireNonNull(numberFormat);
+            return numberFormat::format;
         }
 
         public static LongFunction<String> binary() {
@@ -108,26 +201,6 @@ public final class NumberToStringFunctions {
             return Long::toOctalString;
         }
 
-        public static LongFunction<String> formatted(Locale locale, String format) {
-            Objects.requireNonNull(locale);
-            Objects.requireNonNull(format);
-            return n -> String.format(locale, format, n);
-        }
-
-        public static LongFunction<String> numberFormat(NumberFormat numberFormat) {
-            Objects.requireNonNull(numberFormat);
-            return numberFormat::format;
-        }
-
-        public static LongFunction<String> constant(String constant) {
-            return n -> constant;
-        }
-
-        public static LongFunction<String> supplier(Supplier<String> supplier) {
-            Objects.requireNonNull(supplier);
-            return n -> supplier.get();
-        }
-
     }
 
     public static final class BigIntegerToStringFunctions {
@@ -150,7 +223,6 @@ public final class NumberToStringFunctions {
         /**
          * @see java.util.HexFormat
          */
-
         public static Function<BigInteger, String> hexFormat(HexFormat hexFormat) {
             Objects.requireNonNull(hexFormat);
             return n -> hexFormat.formatHex(n.toByteArray());
@@ -158,26 +230,6 @@ public final class NumberToStringFunctions {
 
         public static Function<BigInteger, String> octal() {
             return n -> n.toString(RADIX_OCTAL);
-        }
-
-        public static Function<BigInteger, String> formatted(Locale locale, String format) {
-            Objects.requireNonNull(locale);
-            Objects.requireNonNull(format);
-            return n -> String.format(locale, format, n);
-        }
-
-        public static Function<BigInteger, String> numberFormat(NumberFormat numberFormat) {
-            Objects.requireNonNull(numberFormat);
-            return numberFormat::format;
-        }
-
-        public static Function<BigInteger, String> constant(String constant) {
-            return n -> constant;
-        }
-
-        public static Function<BigInteger, String> supplier(Supplier<String> supplier) {
-            Objects.requireNonNull(supplier);
-            return n -> supplier.get();
         }
 
     }
