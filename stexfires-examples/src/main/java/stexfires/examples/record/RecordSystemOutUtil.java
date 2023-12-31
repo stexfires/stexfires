366b15ef56bc03f5bfa13629fd2599a21f0aaff3
mkalb <mkalb@mkalb.de>
2023-12-31 13:28:33 +0100

    Fix nullable, null checks and bugs for RecordSystemOutUtil

--- stexfires-examples/src/main/java/stexfires/examples/record/RecordSystemOutUtil.java
+++ stexfires-examples/src/main/java/stexfires/examples/record/RecordSystemOutUtil.java
@@ -1,7 +1,6 @@
 package stexfires.examples.record;
 
 import org.jspecify.annotations.Nullable;
-import stexfires.record.TextFields;
 import stexfires.record.TextRecord;
 import stexfires.record.consumer.SystemOutConsumer;
 import stexfires.record.logger.SystemOutLogger;
@@ -11,7 +10,7 @@ import stexfires.record.message.ShortMessage;
 import stexfires.util.Strings;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.Collection;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Stream;
@@ -20,40 +19,42 @@ import java.util.stream.Stream;
 public final class RecordSystemOutUtil {
 
     public static final RecordMessage<TextRecord> RECORD_MESSAGE =
-            new ShortMessage<>()
-                    .append(" [",
-                            new JoinedTextsMessage<>(", "))
-                    .append("]");
-    public static final SystemOutLogger<TextRecord> RECORD_LOGGER = new SystemOutLogger<>(RECORD_MESSAGE);
-    public static final SystemOutConsumer<TextRecord> RECORD_CONSUMER = new SystemOutConsumer<>(RECORD_MESSAGE);
+            new ShortMessage<>().append(" [", new JoinedTextsMessage<>(", "))
+                                .append("]");
+    public static final SystemOutLogger<TextRecord> RECORD_LOGGER =
+            new SystemOutLogger<>(RECORD_MESSAGE);
+    public static final SystemOutConsumer<TextRecord> RECORD_CONSUMER =
+            new SystemOutConsumer<>(RECORD_MESSAGE);
 
     private RecordSystemOutUtil() {
     }
 
     public static void printlnRecord(TextRecord record) {
+        Objects.requireNonNull(record);
         RECORD_LOGGER.log(record);
     }
 
-    public static void printlnOptionalRecord(Optional<? extends TextRecord> record) {
-        record.ifPresentOrElse(RECORD_LOGGER::log, () -> System.out.println("Optional TextRecord is NULL!"));
+    public static void printlnOptionalRecord(Optional<? extends TextRecord> optionalRecord) {
+        Objects.requireNonNull(optionalRecord);
+        optionalRecord.ifPresentOrElse(RECORD_LOGGER::log, () -> System.out.println("Optional TextRecord is NULL!"));
     }
 
-    public static void printlnRecordList(@Nullable List<? extends TextRecord> records) {
-        if (records == null) {
-            System.out.println("<<<List<TextRecord> is NULL!");
+    public static void printlnRecordCollection(@Nullable Collection<? extends TextRecord> recordCollection) {
+        if (recordCollection == null) {
+            System.out.println("<<<Collection<TextRecord> is NULL!");
         } else {
-            System.out.println("<<<List size=" + records.size());
-            records.forEach(RECORD_CONSUMER::consume);
+            System.out.println("<<<Collection size=" + recordCollection.size());
+            recordCollection.forEach(RECORD_CONSUMER::consume);
         }
         System.out.println(">>>");
     }
 
-    public static void printlnRecordStream(@Nullable Stream<? extends TextRecord> records) {
-        if (records == null) {
+    public static void printlnRecordStream(@Nullable Stream<? extends TextRecord> recordStream) {
+        if (recordStream == null) {
             System.out.println("<<<Stream<TextRecord> is NULL!");
         } else {
             System.out.println("<<<Stream");
-            records.forEachOrdered(RECORD_CONSUMER::consume);
+            recordStream.forEachOrdered(RECORD_CONSUMER::consume);
         }
         System.out.println(">>>");
     }
@@ -65,7 +66,7 @@ public final class RecordSystemOutUtil {
         System.out.println("arrayOfFields:             " + Arrays.toString(record.arrayOfFields()));
         System.out.println("listOfFields:              " + record.listOfFields());
         System.out.println("listOfFieldsReversed:      " + record.listOfFieldsReversed());
-        System.out.println("streamOfFields:            " + TextFields.collectFields(record.streamOfFields()));
+        System.out.println("streamOfFields:            " + record.streamOfFields().toList());
         System.out.println("streamOfTexts:             " + Strings.join(record.streamOfTexts()));
         System.out.println("streamOfTextsAsOptional:   " + Strings.join(record.streamOfTextsAsOptional().flatMap(Optional::stream)));
         System.out.println("category:                  " + record.category());
@@ -82,12 +83,14 @@ public final class RecordSystemOutUtil {
         System.out.println("size:                      " + record.size());
         System.out.println("isNotEmpty:                " + record.isNotEmpty());
         System.out.println("isEmpty:                   " + record.isEmpty());
-        System.out.println("isValidIndex:              " + record.isValidIndex(0));
-        System.out.println("fieldAt:                   " + record.fieldAt(0));
+        System.out.println("isValidIndex(0):           " + record.isValidIndex(0));
+        System.out.println("fieldAt(0):                " + record.fieldAt(0));
+        System.out.println("fieldAtAsOptional(0):      " + record.fieldAtAsOptional(0));
         System.out.println("firstField:                " + record.firstField());
         System.out.println("lastField:                 " + record.lastField());
-        System.out.println("textAt:                    " + record.textAt(0));
-        System.out.println("textAtOrElse:              " + record.textAtOrElse(0, "else"));
+        System.out.println("textAt(0):                 " + record.textAt(0));
+        System.out.println("textAtOrElse(0):           " + record.textAtOrElse(0, "else"));
+        System.out.println("textAtAsOptional(0):       " + record.textAtAsOptional(0));
         System.out.println("firstText:                 " + record.firstText());
         System.out.println("lastText:                  " + record.lastText());
         System.out.println("hashCode:                  " + record.hashCode());
